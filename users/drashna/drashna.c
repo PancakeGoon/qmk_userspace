3b36fa105b591f94ec5748e378a1b9947ef5924b
Drashna Jael're <drashna@live.com>
2024-01-21 10:35:16 -0800

    Revert "[OS Detection] Enable additional OSes to be detected"
    This reverts commit 58b2b351bed2436bd5fcb5d4c913b07d365ecdc3.

--- users/drashna/drashna.c
+++ users/drashna/drashna.c
@@ -235,20 +235,27 @@ uint32_t startup_exec(uint32_t trigger_time, void *cb_arg) {
                 case OS_WINDOWS:
                     xprintf("Windows Detected\n");
                     break;
+#    if 0
                 case OS_WINDOWS_UNSURE:
                     xprintf("Windows? Detected\n");
                     break;
+#    endif
                 case OS_MACOS:
                     xprintf("MacOS Detected\n");
                     break;
                 case OS_IOS:
                     xprintf("iOS Detected\n");
                     break;
+#    if 0
                 case OS_PS5:
                     xprintf("PlayStation 5 Detected\n");
                     break;
                 case OS_HANDHELD:
-                    xprintf("Nintendo Switch/Quest 2 Detected\n");
+                    xprintf("Nintend Switch/Quest 2 Detected\n");
+                    break;
+#    endif
+                default:
+                    xprintf("Unknown OS Detected\n");
                     break;
             }
         }
